e340c751e0118082eae8f632d1d52f4addbc5610
RehanQazi02 <116943445+RehanQazi02@users.noreply.github.com>
2022-10-30 21:50:50 +0530

    Lstm model added

--- lstm.py
+++ lstm.py
@@ -4,30 +4,32 @@ from keras.models import Sequential
 
 
 def build_improved_model(input_dim, output_dim, return_sequences):
-   
+    """
+    Builds an improved Long Short term memory model using keras.layers.recurrent.lstm
+    :param input_dim: input dimension of model
+    :param output_dim: ouput dimension of model
+    :param return_sequences: return sequence for the model
+    :return: a 3 layered LSTM model
+    """
+    model = Sequential()
+    model.add(LSTM(
+        input_shape=(None, input_dim),
+        units=output_dim,
+        return_sequences=return_sequences))
 
+    model.add(Dropout(0.2))
 
+    model.add(LSTM(
+        128,
+        return_sequences=False))
 
+    model.add(Dropout(0.2))
 
+    model.add(Dense(
+        units=1))
+    model.add(Activation('linear'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return model
 
 
 
